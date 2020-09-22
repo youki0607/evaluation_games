b4657aee49c1bd887800889a173c11ebea5428fa
youki0607 <its_showtime@i.softbank.jp>
2020-09-22 23:13:00 +0900

    部分テンプレート追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index, expect: [:index, :show]
 
   def index
-    @tweets = Tweet.includes(:user)
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
