9ede41910d75160c15cab75de7debc66d5a4bba1
youki0607 <its_showtime@i.softbank.jp>
2020-09-22 19:15:59 +0900

    showアクション追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
   def index
     @tweets = Tweet.all
   end
@@ -17,7 +18,6 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
@@ -25,9 +25,16 @@ class TweetsController < ApplicationController
     tweet.update(tweet_params)
   end
 
+  def show
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :explanation)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 
 end
