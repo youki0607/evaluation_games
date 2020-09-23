5e38d4fd69307c3713f02fd41bab3032a8412f29
youki0607 <its_showtime@i.softbank.jp>
2020-09-23 14:37:50 +0900

    controller追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,9 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index, :show, :search]
 
   def index
-    @tweets = Tweet.includes(:user).order("created_at DESC")
+    # @tweets = Tweet.includes(:user).order("created_at DESC")
+    query = "SELECT * FROM tweets"
+    @tweets = Tweet.find_by_sql(query)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'tweets#Index'
+  root to: 'tweets#index'
   resources :tweets do
     resources :comments, only: :create
     collection do
